d6ce11fb0d1b6960a2a3e5b482ecc68050f69205
sangram kesari ray <shankar.ray030@gmail.com>
2014-09-28 05:00:11 -0700

    Initial Commig

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsBrushup::Application.config.secret_token = 'c96832e074a63f63e1692a956ba85bc423252d9f22a785d03cfa850803d66a7d383f825bcd072fa7e894ad0cd99be4083407cd82fa819b2e36eaf25d3f2707cf'
